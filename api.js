415201724118b809cc054f4834711e6228958ae1
Aldana Damilano <dromilano@icloud.com>
2022-07-05 20:03:12 -0300

    Polished styles and visual

--- api.js
+++ api.js
@@ -1,30 +1,3 @@
-
-//Para mostrar el fetch como lo hicimos en clase (no es nada fancy, cat facts que se traducen, no es la mejor calidad esa API)
-
-// getFact();
-
-// function getFact() {
-//     let container = document.getElementById("appContainer");
-//     fetch(`https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1`)
-//         .then(res => res.json())
-//         .then(data => {
-//             let fact = data;
-
-//             let apiUrl = `https://api.mymemory.translated.net/get?q=${fact.text}&langpair=en|es`;
-//             fetch(apiUrl)
-//             .then(res => res.json())
-//             .then(data => {
-//             let translation = data;
-
-//             let text = document.createElement('p');
-        
-//             text.innerHTML = `${translation.responseData.translatedText}`;
-//             container.appendChild(text);
-//         })
-//         })
-//          // .catch(err => console.log(err));
-// };
-
 // API para mostrar listados de ofertas de empleo
 
 let url = "https://jooble.org/api/";
@@ -41,15 +14,51 @@ http.onreadystatechange = function() {
 		let jobList =(JSON.parse(http.responseText));
         let jobs = jobList.jobs;
         jobs.forEach((job)=>{
-            const container = document.getElementById("appContainer")
+            const container = document.getElementById("joblist")
             const titles = document.createElement("h2");
-            titles.innerHTML = `<a href="${job.link}" target="_blank"><h4>${job.title}</h4></a>`;     
+            titles.innerHTML = `</br><a href="${job.link}" target="_blank"><h4>${job.title}</h4></a></br>`;     
             container.appendChild(titles);
             const body = document.createElement("p");
-            body.innerText = `${job.snippet}`;     
+            body.innerHTML = `${job.snippet}`;     
             container.appendChild(body);           
        })
 	}
 }
 http.send(params);
 
+
+//API Weather (using fetch)
+
+// Declaring the variables
+let lon;
+let lat;
+let temperature = document.querySelector(".temp");
+let summary = document.querySelector(".summary");
+let loc = document.querySelector(".location");
+let icon = document.querySelector(".icon");
+const kelvin = 273;
+
+//Getting the location
+window.addEventListener("load", () => {
+if (navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition((position) => {
+	lon = position.coords.longitude;
+	lat = position.coords.latitude;
+	const key = "36f68383de53b2227f6e94d1996ee330";
+	const url =`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&`+`lon=${lon}&appid=${key}`;
+
+	// Calling the API
+	fetch(url)
+		.then((response) => {
+		return response.json();
+		})
+		.then((data) => {
+            console.log(data)
+            temperature.textContent = Math.floor(data.main.temp - kelvin) + "°C |";
+            summary.textContent = data.weather[0].description + " | ";
+            loc.textContent = data.name + " | " + data.sys.country;
+            icon.innerHTML = `<img class="city-icon" src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`;
+		});
+	});
+}
+});
--- index.js
+++ index.js
@@ -263,7 +263,30 @@ function createTimeLine(){
     });
 };
 
- 
 
 
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("jobBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
